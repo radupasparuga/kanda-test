9701fff480bd2f15f37e2f6e3a92ceace9bf20d8
radupasparuga <radupasparuga25@gmail.com>
2020-04-17 17:49:53 +0300

    Added basic components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import InputField from "./InputField.js";
+import Button from "./Button.js";
 
 const Form = () => {
   return(
     <div className="card mx-auto">
       <div className="card-body">
         <h3 className="card-title">Kanda Exam</h3>
-        <InputField label="test"/>
+        <InputField label="First Name"/>
+        <InputField label="Last Name"/>
+        <InputField label="Email"/>
+        <InputField label="Password"/>
+        <InputField label="Confirm Password"/>
+        <Button label="Submit" styling="btn btn-primary"/>
       </div>
     </div>
   )
