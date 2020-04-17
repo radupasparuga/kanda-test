cd0b8a950294d8d846972da65c3be41081083837
radupasparuga <radupasparuga25@gmail.com>
2020-04-17 17:15:32 +0300

    Added small styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Form from './Form.js'
+
 import '../styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div className="App container">
+      <div className="row">
+        <div className="col-sm-12 my-auto">
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Form() {
+  return(
+    <div className="card mx-auto">
+      Form
+    </div>
+  )
+}
+
+export default Form;
